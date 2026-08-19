cd5b963b45c3aa80a5f0713c5215ae3bf0cb7009
nyamyjese <hei.jese.2@gmail.com>
2026-08-19 14:37:42 +0100

    feat(controller): add StudentController

---
+++ src/main/java/com/hei/school/endpoint/rest/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.hei.school.endpoint.rest.controller;
+
+import com.hei.school.service.diploma.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping("/{studentId}/graduation-status")
+    public boolean checkGraduationStatus(
+            @PathVariable UUID studentId,
+            @RequestParam UUID trackId
+            ) {
+
+        return studentService.isGraduated(studentId, trackId);
+    }
+
+    @GetMapping("/{studentId}/year-validation")
+    public boolean checkYearValidation(
+            @PathVariable UUID studentId,
+            @RequestParam UUID trackId,
+            @RequestParam int academicYear
+    ) {
+
+        return studentService.isYearValidated(studentId, trackId, academicYear);
+    }
+}
