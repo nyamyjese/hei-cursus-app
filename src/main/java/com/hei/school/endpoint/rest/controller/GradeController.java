e9377f2c2c537f6580d1edededd70b1656792cc6
nyamyjese <hei.jese.2@gmail.com>
2026-08-19 14:39:18 +0100

    chore: format

--- src/main/java/com/hei/school/endpoint/rest/controller/GradeController.java
+++ src/main/java/com/hei/school/endpoint/rest/controller/GradeController.java
@@ -2,29 +2,26 @@ package com.hei.school.endpoint.rest.controller;
 
 import com.hei.school.model.Grade;
 import com.hei.school.service.diploma.GradeService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/courses/{courseId}/grades")
 @RequiredArgsConstructor
-
 public class GradeController {
 
-    private final GradeService gradeService;
+  private final GradeService gradeService;
 
-    @PutMapping("/{gradeId}")
-    public Grade updateGrade(
-            @PathVariable UUID courseId,
-            @PathVariable UUID gradeId,
-            @RequestParam BigDecimal newValue,
-            @RequestParam String reason ,
-            @RequestParam UUID teacherId
-            ){
+  @PutMapping("/{gradeId}")
+  public Grade updateGrade(
+      @PathVariable UUID courseId,
+      @PathVariable UUID gradeId,
+      @RequestParam BigDecimal newValue,
+      @RequestParam String reason,
+      @RequestParam UUID teacherId) {
 
-        return gradeService.updateGrade(gradeId, newValue, reason, teacherId);
-    }
+    return gradeService.updateGrade(gradeId, newValue, reason, teacherId);
+  }
 }
--- src/main/java/com/hei/school/endpoint/rest/controller/StudentController.java
+++ src/main/java/com/hei/school/endpoint/rest/controller/StudentController.java
@@ -1,35 +1,27 @@
 package com.hei.school.endpoint.rest.controller;
 
 import com.hei.school.service.diploma.StudentService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/students")
 @RequiredArgsConstructor
-
 public class StudentController {
 
-    private final StudentService studentService;
+  private final StudentService studentService;
 
-    @GetMapping("/{studentId}/graduation-status")
-    public boolean checkGraduationStatus(
-            @PathVariable UUID studentId,
-            @RequestParam UUID trackId
-            ) {
+  @GetMapping("/{studentId}/graduation-status")
+  public boolean checkGraduationStatus(@PathVariable UUID studentId, @RequestParam UUID trackId) {
 
-        return studentService.isGraduated(studentId, trackId);
-    }
+    return studentService.isGraduated(studentId, trackId);
+  }
 
-    @GetMapping("/{studentId}/year-validation")
-    public boolean checkYearValidation(
-            @PathVariable UUID studentId,
-            @RequestParam UUID trackId,
-            @RequestParam int academicYear
-    ) {
+  @GetMapping("/{studentId}/year-validation")
+  public boolean checkYearValidation(
+      @PathVariable UUID studentId, @RequestParam UUID trackId, @RequestParam int academicYear) {
 
-        return studentService.isYearValidated(studentId, trackId, academicYear);
-    }
+    return studentService.isYearValidated(studentId, trackId, academicYear);
+  }
 }
