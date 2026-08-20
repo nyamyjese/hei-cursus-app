4d6bab72253c94f48efec77dad066adb03200ac5
nyamyjese <hei.jese.2@gmail.com>
2026-08-20 22:36:49 +0100

    fix(test): fix GradeControllerIT , add StudentControllerIT and DiplomaControllerIT

--- src/test/java/com/hei/school/endpoint/rest/controller/GradeControllerIT.java
+++ src/test/java/com/hei/school/endpoint/rest/controller/GradeControllerIT.java
@@ -5,34 +5,34 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.hei.school.conf.TestcontainersInitializer;
 import com.hei.school.model.Grade;
 import com.hei.school.service.diploma.GradeService;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest(controllers = GradeController.class)
 @AutoConfigureMockMvc(addFilters = false)
-@ContextConfiguration(initializers = TestcontainersInitializer.class)
 public class GradeControllerIT {
 
-    @Autowired MockMvc mockMvc;
-    @MockBean GradeService gradeService;
+  @Autowired MockMvc mockMvc;
 
-    @Test
-    void grade_controller_updateGrade_is_ok() throws Exception {
-        when(gradeService.updateGrade(any(), any(), any(), any())).thenReturn(new Grade());
+  @MockBean GradeService gradeService;
 
-        mockMvc.perform(put("/courses/" + UUID.randomUUID() + "/grades/" + UUID.randomUUID())
-                        .param("newValue", "15.00")
-                        .param("reason", "reclamation")
-                        .param("teacherId", UUID.randomUUID().toString()))
-                .andExpect(status().isOk());
-    }
+  @Test
+  void grade_controller_updateGrade_is_ok() throws Exception {
+    when(gradeService.updateGrade(any(), any(), any(), any())).thenReturn(new Grade());
+
+    mockMvc
+        .perform(
+            put("/courses/" + UUID.randomUUID() + "/grades/" + UUID.randomUUID())
+                .param("newValue", "15.00")
+                .param("reason", "reclamation")
+                .param("teacherId", UUID.randomUUID().toString()))
+        .andExpect(status().isOk());
+  }
 }
